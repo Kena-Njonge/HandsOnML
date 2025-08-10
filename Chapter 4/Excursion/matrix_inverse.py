8281a73cdd679a8258fbac4d9c7c5f9980e6a477
Kena Njonge <kenakiruri@gmail.com>
2025-08-10 23:40:42 +0200

    Middy Commity, need to add gradient descent and softmax

---
+++ Chapter 4/Excursion/matrix_inverse.py
@@ -0,0 +1,6 @@
+# Given a matrix of shape (n,n) calculate its inverse
+#
+# The matrix is invertible
+# Write a function to perform this calcualtion
+# What is the compuational complexity with regard to the number of features (coulumns)?
+
