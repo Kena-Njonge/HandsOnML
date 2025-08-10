2cf5a2ad62355d612fe69757efe347b442e845f4
Kena-Njonge <75584199+Kena-Njonge@users.noreply.github.com>
2025-08-10 23:42:58 +0200

    Delete Chapter 4/Excursion/matrix_inverse.py

--- Chapter 4/Excursion/matrix_inverse.py
+++
@@ -1,6 +0,0 @@
-# Given a matrix of shape (n,n) calculate its inverse
-#
-# The matrix is invertible
-# Write a function to perform this calcualtion
-# What is the compuational complexity with regard to the number of features (coulumns)?
-
